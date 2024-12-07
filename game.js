46cd0e3f504f261cf1e9d88d4b8be1cd42b15efb
AbbyJ22 <abbyjohnson040@gmail.com>
2024-12-07 12:55:23 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,27 +1,39 @@
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
+
         var game = new PixelJS.Engine();
+        
+       
         game.init({
             container: 'game_container',
             width: 800,
             height: 600
         });
-        
+
+        // Run the game loop
         game.run(function (elapsedTime, dt) {
+
         });
 
+
         var backgroundLayer = game.createLayer('background');
-var fence = backgroundLayer.createEntity();
-backgroundLayer.static = true;
-fence.pos = { x: 0, y: 0 };
-fence.asset = new PixelJS.Tile();
-fence.asset.prepare({
-    name: 'fence.png',
-    size: { 
-        width: 800, 
-        height: 600 
-    }
-});
+        
+
+        var fence = backgroundLayer.createEntity();
+        
+
+        backgroundLayer.static = true;
+        
+    
+        fence.pos = { x: 0, y: 0 };
+
+
+        fence.asset = new PixelJS.Tile();
+        fence.asset.prepare({
+            name: 'fence.png'{ 
+                width: 800,  
+                height: 600  
+            }
+        });
     }
 }
-
