3f8d2f97019f4caa2e10c304239773b184ecc189
AbbyJ22 <abbyjohnson040@gmail.com>
2024-12-08 13:21:25 -0500

    .

---
+++ game.js
@@ -0,0 +1,23 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets
+    this.load.image('fence', 'assets/fence.png');
+}
+
+function create() {
+    // Add background
+    this.add.image(400, 300, 'fence');
+}
