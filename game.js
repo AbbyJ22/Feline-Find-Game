b5d45ab9d1d9f992da2301da8fe2cbf6506f0a47
AbbyJ22 <abbyjohnson040@gmail.com>
2024-12-08 19:30:18 -0500

    Update game.js

--- game.js
+++ game.js
@@ -18,18 +18,18 @@ function preload() {
 }
 
 function create() {
-    // Add the background image
+
     const bg = this.add.image(0, 0, 'fence');
 
-    // Set the display size of the background image (scaling to 1000x1000)
+  
     const displayWidth = 307;
     const displayHeight = 101;
     bg.setDisplaySize(displayWidth, displayHeight);
 
-    // Update the Phaser game canvas size to match the scaled image size
+
     this.game.scale.resize(displayWidth, displayHeight);
 
-    // Center the background image on the resized canvas
+
     bg.setPosition(displayWidth / 2, displayHeight / 2);
 
     console.log("Canvas resized");
