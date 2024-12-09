4ed986977f7033cd00810818e074abc1fe7445a1
AbbyJ22 <abbyjohnson040@gmail.com>
2024-12-08 20:09:48 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 800, // Visible canvas width
+    height: 600, // Visible canvas height
     parent: 'game-container',
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     },
     pixelArt: true,
 };
 
 const game = new Phaser.Game(config);
+let bg;
 
 function preload() {
     // Load assets
@@ -18,19 +20,33 @@ function preload() {
 }
 
 function create() {
+    // Add the background image (position it at 0, 0 relative to world coordinates)
+    bg = this.add.image(0, 0, 'fence').setOrigin(0);
 
-    const bg = this.add.image(0, 0, 'fence');
-
-  
+    // Scale the background to a larger size
     const displayWidth = 970;
     const displayHeight = 320;
     bg.setDisplaySize(displayWidth, displayHeight);
 
+    // Set the world bounds to match the background size
+    this.physics.world.setBounds(0, 0, displayWidth, displayHeight);
 
-    this.game.scale.resize(displayWidth, displayHeight);
+    // Make the camera follow the player or move manually
+    this.cameras.main.setBounds(0, 0, displayWidth, displayHeight);
 
+    // Position the camera in the middle of the background initially
+    this.cameras.main.scrollX = displayWidth / 2 - config.width / 2;
 
-    bg.setPosition(displayWidth / 2, displayHeight / 2);
+    // Add keyboard input for manual scrolling (for now)
+    this.cursors = this.input.keyboard.createCursorKeys();
+}
 
-    console.log("Canvas resized");
+function update() {
+    // Check for left/right input to scroll the camera
+    if (this.cursors.left.isDown) {
+        this.cameras.main.scrollX -= 5; // Move camera left
+    }
+    if (this.cursors.right.isDown) {
+        this.cameras.main.scrollX += 5; // Move camera right
+    }
 }
