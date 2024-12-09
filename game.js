d2537bf6b28a0752e443f2f239b11737539324ef
AbbyJ22 <abbyjohnson040@gmail.com>
2024-12-08 22:02:19 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const config = {
     type: Phaser.AUTO,
     width: 657, // Visible canvas width
-    height: 526, // Visible canvas height
+    height: 500, // Visible canvas height
     parent: 'game-container',
     scene: {
         preload: preload,
